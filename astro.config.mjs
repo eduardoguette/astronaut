7eefe61e76e366cb30c6421d55d107507483fdba
Eduardo Guette <eduardoguette@gmail.com>
2023-12-07 21:57:39 +0100

    feature: implemented analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
-import vercel from '@astrojs/adapter-vercel'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import { defineConfig } from 'astro/config'
 
+import vercel from "@astrojs/vercel/serverless"
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://astronaut-xi.vercel.app',
-	integrations: [mdx(), sitemap()],
-	output: 'server',
+  site: 'https://astronaut-xi.vercel.app',
+  integrations: [mdx(), sitemap()],
+  output: 'server',
 	adapter: vercel({
     webAnalytics: {
       enabled: true,
